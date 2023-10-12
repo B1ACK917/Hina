093be1d3a4bc177f1a9f79235ee29a234a6a2b2d
B1ACK917 <397615103@qq.com>
2023-10-12 15:44:14 +0800

    update: fix dump process bug
    update: fix dump process bug
    
    update: fix dump process bug
    
    update: fix dump process bug

--- src/event/process.rs
+++ src/event/process.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::path::PathBuf;
 
 use crate::core::func;
@@ -90,6 +91,15 @@ pub fn dump_proc(user: &String,
                  work_path: &PathBuf,
                  input_path: &PathBuf) {
     let mut target = func::get_execute_target(work_path, input_path);
+    if target.exists() {
+        let is_some = target.read_dir().unwrap().next().is_some();
+        if is_some {
+            println!("Directory not empty");
+            return;
+        }
+    } else {
+        fs::create_dir_all(&target).unwrap();
+    }
     let all_process = get_all_process();
     let user_process: Vec<&ProcessBlock> = all_process
         .iter()
@@ -99,11 +109,12 @@ pub fn dump_proc(user: &String,
         dbg!(&user_process);
     }
     for process in user_process {
-        let uid = &process.uid;
-        target.push(uid);
-        let command = String::from(format!("cat /proc/{}/smaps > {}/smaps", uid, target.display()));
+        let pid = &process.pid;
+        target.push(pid.to_string());
+        fs::create_dir(&target).unwrap();
+        let command = String::from(format!("cat /proc/{}/smaps > {}/smaps", pid, &target.display()));
         func::execute_command(&command);
-        let command = String::from(format!("cat /proc/{}/cmdline > {}/cmdline", uid, target.display()));
+        let command = String::from(format!("cat /proc/{}/cmdline > {}/cmdline", pid, &target.display()));
         func::execute_command(&command);
         target.pop();
     }
